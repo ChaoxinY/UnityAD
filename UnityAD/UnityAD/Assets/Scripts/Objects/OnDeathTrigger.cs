5a8668f58e75d99c317a7ab2f891d417c8ffe6ad
Chaoxin <chaoxin_yang@hotmail.com>
2019-08-11 19:58:55 +0200

    Added two function scripts for the unity project.

---
+++ UnityAD/UnityAD/Assets/Scripts/Objects/OnDeathTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnDeathTrigger :MonoBehaviour
+{
+	#region Variables
+	public GameObject eventSender;
+	public UnityEvent onDeathEvent;
+	#endregion
+
+	#region Initialization
+	#endregion
+
+	#region Functionality
+
+	private void OnDestroy()
+	{
+		if(eventSender.activeInHierarchy)
+		{
+			onDeathEvent.Invoke();
+		}
+	}
+	#endregion
+}
+
+
+
---
+++ UnityAD/UnityAD/Assets/Scripts/Objects/SelfDestructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour
+{
+	#region Variables
+	public float lifeTime;
+	#endregion
+
+	#region Initialization
+	private void Start()
+	{
+		Destroy(gameObject, lifeTime);
+	}
+	#endregion
+
+	#region Functionality
+	#endregion
+}
+
